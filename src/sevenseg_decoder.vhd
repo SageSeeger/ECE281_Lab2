34367f70b24ae15d44847fc3e50d77c3d35d2130
SageSeeger <c28sage.seeger@afacademy.af.edu>
2026-03-01 21:30:38 -0700

    final commit

--- src/sevenseg_decoder.vhd
+++ src/sevenseg_decoder.vhd
@@ -2,7 +2,7 @@
 -- Company: 
 -- Engineer: 
 -- 
--- Create Date: 02/24/2026 02:07:33 PM
+-- Create Date: 02/24/2026 02:00:45 PM
 -- Design Name: 
 -- Module Name: sevenseg_decoder - Behavioral
 -- Project Name: 
@@ -32,13 +32,28 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity sevenseg_decoder is
-    Port ( i_Hex : in STD_LOGIC_VECTOR (3 downto 0);
-           o_seg_n : out STD_LOGIC_VECTOR (6 downto 0));
+Port (i_Hex : in std_logic_vector(3 downto 0);
+      o_seg_n: out std_logic_vector(6 downto 0));
 end sevenseg_decoder;
 
 architecture Behavioral of sevenseg_decoder is
-
 begin
-
-
+    with i_Hex select
+        o_seg_n <= "1000000" when "0000", -- 0
+                   "1111001" when "0001", -- 1
+                   "0100100" when "0010", -- 2
+                   "0110000" when "0011", -- 3
+                   "0011001" when "0100", -- 4
+                   "0010010" when "0101", -- 5
+                   "0000010" when "0110", -- 6
+                   "1111000" when "0111", -- 7
+                   "0000000" when "1000", -- 8
+                   "0011000" when "1001", -- 9
+                   "0001000" when "1010", -- A
+                   "0000011" when "1011", -- B
+                   "0100111" when "1100", -- C
+                   "0100001" when "1101", -- D
+                   "0000110" when "1110", -- E
+                   "0001110" when "1111", -- F
+                   "1111111" when others; -- all other cases
 end Behavioral;
--- src/top_basys3.vhd
+++ src/top_basys3.vhd
@@ -79,9 +79,17 @@ end top_basys3;
 architecture top_basys3_arch of top_basys3 is 
 	
   -- declare the component of your top-level design
+  component sevenseg_decoder
+    port(
+        i_Hex   : in std_logic_vector(3 downto 0);
+        o_seg_n : out std_logic_vector(6 downto 0)
+        );
+        
+    end component;
 
 
   -- create wire to connect button to 7SD enable (active-low)
+  signal w_en : std_logic;
 
   
 begin
@@ -89,13 +97,24 @@ begin
 
 	--	Port map: wire your component up to the switches and seven-segment display cathodes
 	-----------------------------------------------------	
-	
+U1: sevenseg_decoder
+    port map(
+            i_Hex => sw,
+            o_seg_n => seg
+            );
 	
 	-- CONCURRENT STATEMENTS ----------------------------
 	
 	-- wire up active-low 7SD anode (active low) to button (active-high)
 	-- display 7SD 0 only when button pushed
 	-- other 7SD are kept off
+	
+w_en  <= not btnC;
+
+an(0)   <= w_en;
+an(1)   <= '1';
+an(2)   <= '1';
+an(3)   <= '1';
 	-----------------------------------------------------
 	
 end top_basys3_arch;
